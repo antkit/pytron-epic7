ea53a0a7031aa0e3f83cdf43da7bbf72afe40d7c
qianzhaoyi <qianzhaoyi@outlook.com>
2022-11-29 11:34:57 +0800

    update code using the latest electron template

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,42 +1,57 @@
 // Native
-import { join } from 'path'
-import { format } from 'url'
+import * as path from "path";
 
 // Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron'
-import isDev from 'electron-is-dev'
-import prepareNext from 'electron-next'
+import { BrowserWindow, app, ipcMain, IpcMainEvent } from "electron";
+import isDev from "electron-is-dev";
+import prepareNext from "electron-next";
 
-// Prepare the renderer once the app is ready
-app.on('ready', async () => {
-  await prepareNext('./renderer')
-
+function createWindow() {
+  // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: false,
-      preload: join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
-  })
+    width: isDev ? 1440 : 800,
+  });
 
-  const url = isDev
-    ? 'http://localhost:8000/'
-    : format({
-        pathname: join(__dirname, '../renderer/out/index.html'),
-        protocol: 'file:',
-        slashes: true,
-      })
+  // and load the index.html of the app.
+  const url = isDev ? "http://localhost:18123/" : "https://epic7.joyqoo.com";
+  mainWindow.loadURL(url);
 
-  mainWindow.loadURL(url)
-})
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+}
 
-// Quit the app once all windows are closed
-app.on('window-all-closed', app.quit)
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(async () => {
+  await prepareNext("./renderer");
+
+  createWindow();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
 
 // listen the channel `message` and resend the received message to the renderer process
-ipcMain.on('message', (event: IpcMainEvent, message: any) => {
-  console.log(message)
-  setTimeout(() => event.sender.send('message', 'hi from electron'), 500)
-})
+ipcMain.on("message", (event: IpcMainEvent, message: any) => {
+  console.log(message);
+  setTimeout(() => event.sender.send("message", "hi from electron"), 500);
+});
